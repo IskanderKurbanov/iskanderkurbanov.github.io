d028ae2e499afddcd96385a624f027f26705c38d
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2019-11-28 23:10:27 +0300

    min commit
    link for friend

---
+++ js/topBt.js
@@ -0,0 +1,14 @@
+var timeOut;
+function goUp() {
+   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+   if(top > 0) {
+      window.scrollBy(0,-100);
+      timeOut = setTimeout('goUp()',20);
+   } else clearTimeout(timeOut);
+}
+window.onscroll = function() {
+   var scrollElem = document.getElementById("scrollToTop");
+   if (document.body.scrollTop > document.documentElement.clientHeight) {
+      scrollElem.style.opacity = "1";
+   }
+}

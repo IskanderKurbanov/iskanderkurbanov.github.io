08e9e0ac620e074a284aaef85748f477d207689a
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2020-01-07 18:48:44 +0300

    commit for next fix or not.
    some intresting add

--- js/scroll.js
+++ js/scroll.js
@@ -5,4 +5,25 @@
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
+    $("#Smenu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+    $("#NextDoor").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+    $("#scrollToTop").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });

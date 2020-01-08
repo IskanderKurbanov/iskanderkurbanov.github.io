15c90a86b42d0732c4a3c84edacaace21b5783a6
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2020-01-08 16:06:25 +0300

    some visual fix
    not good, not bad

--- js/jsFuncs.js
+++ js/jsFuncs.js
@@ -3,17 +3,24 @@ window.onload = HSize;
 setInterval(
 	function() {
   $color = generateColor();
-  $color_1 = generateColor();
+  $color1 = generateColor1();
   $('header').css('background-color', $color);
   $('#arrowTop').css('color', $color);
-  //$('.FSmenu').css('background-color', $color);
-  $('.SSmenu a').css('color', $color_1);
+  $('.SSmenu a').css('color', $color);
+  $('main .galleryBox').css('background', $color1);
+  /*$('main .Profile-info').css('background', $color1);*/
 }, 3000);
 
 function generateColor() {
 	let arr = ['#913f3f', '#5a81bf', '#3eab9c', '#a2ab3e', '#9567bf', '#bab525','#00b59d'];
 	var i = Math.floor(Math.random() * arr.length);
 	return arr[i];
+}
+
+function generateColor1() {
+  let arr = ['edcaca','ceddf5','ace3db','e4e8b7','cab4de','e0dfb1','a3c7c2'];
+  var i = Math.floor(Math.random() * arr.length);
+  return '#' + arr[i];
 }
 
 function HSize() {

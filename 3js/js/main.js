8beb9e45ec53409882b11670d2d8901981cffed2
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2020-10-09 21:27:03 +0300

    new version

--- 3js/js/main.js
+++ 3js/js/main.js
@@ -3,6 +3,7 @@ const screenBlock = document.querySelector('.screen__canvas')
 const screen = document.querySelector('#screen-canvas')
 const controlBtns = document.querySelectorAll('.controlBtn')
 const routeHud = document.querySelector('#screen__route')
+const hideMouse = document.querySelector('.hideMouse')
 
 screen.width = parseInt(getComputedStyle(screen).width)
 screen.height = parseInt(getComputedStyle(screen).height)
@@ -28,8 +29,17 @@ function init() {
 	let turnLeft = 0;
 	let turnRight = 0;
 
-	let speedPlayer = 0.1;
-	let sensityPlayer = 0.1;
+	let speedPlayer = 0.05;
+	let sensityPlayer = 0.09;
+
+	let lock = false;
+	let mouse = { x : parseInt(getComputedStyle(screenBlock).width)/2, y : parseInt(getComputedStyle(screenBlock).height)/2 };
+
+	let collideTuoch = false;
+	let collisionArrey = {}
+	let collisionEl = 0
+
+	let lmove = 0.1
 
 	let scene = new THREE.Scene();
 
@@ -39,9 +49,7 @@ function init() {
 	renderer.setSize( parseInt(getComputedStyle(screenBlock).width), parseInt(getComputedStyle(screenBlock).height));
 
 	let camera = new THREE.PerspectiveCamera( 65, parseInt(getComputedStyle(screenBlock).width) / parseInt(getComputedStyle(screenBlock).height), 0.1, 1000 );
-	camera.position.z = 5
-
-	let v = new THREE.Vector3(0,0,0);
+	camera.position.set(-2,0,5)
 
 	let SFog = new THREE.FogExp2(0xff5993, 0.0001);
 	scene.fog = SFog
@@ -59,7 +67,7 @@ function init() {
 
 	let textureLoader = new THREE.TextureLoader();
 	let geometry = new THREE.BoxGeometry();
-	let material = new THREE.MeshLambertMaterial( { map: textureLoader.load('img/testWall.jpg') } );
+	let material = new THREE.MeshLambertMaterial( { map: textureLoader.load('img/testWall.jpg') /*, wireframe: true*/ } );
 	let retroSunMaterial = new THREE.MeshBasicMaterial( { map: textureLoader.load('img/retroSun.jpg') } );
 	let lampMaterial = new THREE.MeshBasicMaterial( { map: textureLoader.load('img/lamp.jpg') } );
 	//let cube = new THREE.Mesh( geometry, material );
@@ -85,12 +93,17 @@ function init() {
 	lamp.position.set(0,2,2.5);
 	scene.add( lamp );
 
-	let gridFloor = new THREE.GridHelper(100, 20, 0xee00ff, 0xee00ff)
+	let cube = new THREE.Mesh( geometry, material );
+	cube.position.set(-2,2,-2);
+	cube.castShadow = true
+	cube.receiveShadow = true
+	scene.add(cube);
+
+	let gridFloor = new THREE.GridHelper(100, 50, 0xee00ff, 0xee00ff)
 	gridFloor.position.set(0,-0.5,0)
 	scene.add( gridFloor );
 
-	let collisionArrey = {}
-	let collisionEl = 0
+	
 	for(let i = 0; i < map.length; i++){ 
 		for (let j = 0; j < map[i].length; j++){
 			if (map[i][j]) {
@@ -106,113 +119,128 @@ function init() {
 	}
 
 
-	function collisionValidation() {
-		deg = 180/Math.PI*camera.rotation.y
-		for(let key in collisionArrey) {
-			if (collisionArrey[key]['x'] == camera.position.x && collisionArrey[key]['z'] == camera.position.z) {
-				return false
-			}
-		}
-		return true
+	console.log(collisionArrey)
+	for (var i = collisionArrey.length - 1; i >= 0; i--) {
+		collisionArrey[i]
+	}
+	
+
+	function collision() {
+
+		if (camera.position.x > 5) collideTuoch = true
+		else collideTuoch = false
+		
 	}
 	
 	
-	/*
 	controlBtns.forEach( item => {
-		item.addEventListener('click', () => {
+		item.addEventListener('mousedown', () => {
 			let move = item.getAttribute('data')
-			if (move === 'right') {}
-			else if (move === 'left') {}
-			else if (move === 'up') {}
-			else if (move === 'down') {}
-			else if (move === 'turnLeft') {}
-			else if (move === 'turnRight') {}
+			if (move === 'right') PressRight = speedPlayer;
+			else if (move === 'left') PressLeft = speedPlayer;
+			else if (move === 'up') PressForward = speedPlayer;
+			else if (move === 'down') PressBack = speedPlayer;
+			else if (move === 'turnLeft') turnLeft = sensityPlayer;
+			else if (move === 'turnRight') turnRight = sensityPlayer;
 		})
-	})*/
+	})
+	controlBtns.forEach( item => {
+		item.addEventListener('mouseup', () => {
+			PressBack = PressForward = PressLeft = PressRight = PressUp = turnLeft = turnRight = 0;
+		})
+	})
 
 
 	document.addEventListener('keydown', function(event) {
 		if (event.code == 'KeyW' || event.code == 'ArrowUp') {
-			//camera.position.x -= Math.sin(camera.rotation.y) *0.1
-			//camera.position.z -= Math.cos(camera.rotation.y) *0.1
 			PressForward = speedPlayer;
+			//camera.position.x -= Math.sin(camera.rotation.y) *speedPlayer
+			//camera.position.z -= Math.cos(camera.rotation.y) *speedPlayer
 		}
 		else if (event.code == 'KeyS' || event.code == 'ArrowDown') PressBack = speedPlayer;
 		else if (event.code == 'KeyD' || event.code == 'ArrowRight') PressRight = speedPlayer;
 		else if (event.code == 'KeyA' || event.code == 'ArrowLeft') PressLeft = speedPlayer;
-		else if (event.code == 'KeyA' || event.code == 'ArrowLeft') PressUp = speedPlayer;
-		else if (event.code == 'KeyQ') turnRight = sensityPlayer;
-		else if (event.code == 'KeyE') turnLeft = sensityPlayer;
+		//else if (event.code == 'KeyA' || event.code == 'ArrowLeft') PressUp = speedPlayer;
+		else if (event.code == 'KeyQ') turnLeft = sensityPlayer;
+		else if (event.code == 'KeyE') turnRight = sensityPlayer;
 	});
 	document.addEventListener('keyup', function(event) {
 		if (event.code == 'KeyW' || event.code == 'ArrowUp') PressForward = 0
 		else if (event.code == 'KeyS' || event.code == 'ArrowDown') PressBack = 0
 		else if (event.code == 'KeyD' || event.code == 'ArrowRight') PressRight = 0;
 		else if (event.code == 'KeyA' || event.code == 'ArrowLeft') PressLeft = 0;
-		else if (event.code == 'KeyQ') turnRight = 0;
-		else if (event.code == 'KeyE') turnLeft = 0;
+		else if (event.code == 'KeyQ') turnLeft = 0;
+		else if (event.code == 'KeyE') turnRight = 0;
 	});
 
 
-	screen.addEventListener('mousemove', (e)=>{
-		if (180/Math.PI*camera.rotation.y >= 360 || 180/Math.PI*camera.rotation.y <= -360) camera.rotation.y = 0
-		const movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0;
-		const movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0;
-		const my = movementX * Math.PI / 180;
-		//const mx = movementY * Math.PI / 180;
-		camera.rotation.y -= my;
+	document.addEventListener('mousemove', (e)=>{
+		if (lock) {
+			const movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0;
+			const movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0;
+			const my = movementX * Math.PI / 180;
+			//const mx = movementY * Math.PI / 180;
+			camera.rotation.y -= my;
+		}
 	}, true)
-
-	let cube = new THREE.Mesh( geometry, material );
-	cube.position.set(-2,2,-2);
-	cube.castShadow = true
-	cube.receiveShadow = true
-	scene.add(cube);
+	document.addEventListener("pointerlockchange", (event)=>{
+		lock = !lock;
+	});
+	hideMouse.onclick = function(){
+		if (!lock) {
+			hideMouse.requestPointerLock();
+		}
+	};
 	
-	let lmove = 0.1
 	let animate = function () {
+		collision()
+
 		requestAnimationFrame( animate );
 		deg = 180/Math.PI*camera.rotation.y
-
-		cube.rotation.x += 0.01;
-		cube.rotation.y += 0.01;
-
+		if (deg >= 360 || deg <= -360) camera.rotation.y = 0
+		
 		let dx =   (PressRight - PressLeft)*Math.cos(camera.rotation.y) - (PressForward - PressBack)*Math.sin(camera.rotation.y);
 		let dz =   - (PressForward - PressBack)*Math.cos(camera.rotation.y) - (PressRight - PressLeft)*Math.sin(camera.rotation.y);
-		let dy = (turnLeft - turnRight)
-		//dx = Math.sin(camera.rotation.y) *PressForward
-		//dz = Math.cos(camera.rotation.y) *PressForward
+		let ry = (turnLeft - turnRight)
 
+		if (collideTuoch) {
+			dx = -dx;
+			dz = -dz;
+		}
+		
 		camera.position.x += dx;
 		camera.position.z += dz;
-		camera.rotation.y -= dy;
+		
 
+		camera.rotation.y += ry;
 
 		if (light1.position.x >= 5)lmove = -0.01
 		if (light1.position.x <= -5)lmove = 0.01
 
 		light1.position.x += lmove
 		lamp.position.x += lmove
-		//retroSun.position.y = lmove
 
-		printRouteHud(camera.position.x,camera.position.z)
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+
+		printRouteHud(camera.position.x,camera.position.z, deg, lock, mouse, dx, dz)
 		renderer.render( scene, camera );
 	};
-
+	
 	animate();
 
 }
 
-function printRouteHud(x,z){
-
-	routeHud.innerText = `x:${x}|z:${z}`
-
-	
+function printRouteHud(x,z,rotY,ml,m_center,dx,dz){
+	x = Math.round(x);
+	z = Math.round(z);
+	rotY = Math.round(rotY);
+	routeHud.innerText = `x:${x}|mouse_lock:${ml}
+	z:${z}|rotY:${rotY}
+	dx:${dx};dz:${dz}
+	screen_center: ${m_center['x']} x ${m_center['y']}
+	`
 }
 
-
-
 init()
-
-
-
+ 

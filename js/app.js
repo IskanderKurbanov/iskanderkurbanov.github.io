f5c1a85cc978975cc8302eb3b743365c51939dde
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2021-04-13 10:52:11 +0300

    v2021.4.496

--- js/app.js
+++ js/app.js
@@ -47,90 +47,148 @@ sendRequest('GET', URL)
     loadContentDone()
   })
 
-
-
 // Easter Egg {amount: 2}
-
-let isWeatherBlock = false
 
 function weatherElement() {
   document.querySelector(".weather_hiden").innerHTML = '<input type="" name="" class="weatherInput" placeholder="write your city">'
   document.querySelector('.weatherInput').addEventListener('keypress', function (e) {
       if (e.key === 'Enter') {
-
           sendRequest('GET', `${worldUrl}${document.querySelector('.weatherInput').value}`)
-            .then(data => createWeatherComponent(data))
-          
-
-          document.querySelector('.weather_hiden').innerHTML = '2020/21 Курбанов Искандер.'
+            .then(data => new WeatherApplet(data))
+          document.querySelector('.weatherInput').value = ''
       }
   })
 }
+document.querySelector(".weather_hiden").addEventListener("dblclick",()=>weatherElement())
+document.querySelector(".weather_hiden").addEventListener('touchend', ()=>weatherElement())
 
-function createWeatherComponent(data) {
-  if(data){
-    dataTime = new Date(data.time)
-    curr_date = dataTime.getDate()
-    curr_month = dataTime.getMonth() + 1
-    curr_year = dataTime.getFullYear()
-    curr_h = dataTime.getHours()
-    curr_m = dataTime.getMinutes()
-    curr_s = dataTime.getSeconds()
-    dateW = curr_date+'/'+curr_month+'/'+curr_year
+class WeatherApplet{
+  constructor(data) {
+
+    this.width = document.body.clientWidth
+
+    this.dataTime = new Date(data.time)
+    
+    this.dataWeather = data.weather
+
+    this.weatherBlock = this.createElement(this.dataWeather, data.city)
+
+    this.timeBlock = this.weatherBlock.querySelector('.timer')
+    this.yearTimeBlock = this.weatherBlock.querySelector('.yearTimer')
+
+    this.dragableFunc = false
+
+    this.weatherBlockHeader = this.weatherBlock.querySelector('.weatherBlockHeader')
+
+    this.timer([this.timeBlock, this.yearTimeBlock], this.dataTime)
+
+    this.headFunc()
+  }
+
+  createElement(data, city){
+    let dragHead
     const weatherBlock = document.createElement('div')
     document.body.append(weatherBlock)
     weatherBlock.className = "weatherBlock"
+    this.width>=1000?dragHead='<article class="anchorDrgBtn"></article>':dragHead=''
     weatherBlock.innerHTML = `
-                            <div style="display: flex;">
+                            <div style="display: flex; class="weatherBlockHeader">
                               <article class="anchorExtBtn"></article>
+                              ${dragHead}
                               <article class="anchorBtn"></article>
                             </div>
                             <div style="display: flex;margin-bottom:10px;">
                             <img src="${data.icon}" alt="${data.main}" style="width:80px;height:80px;">
                             <aside>
-                            <p style="font-size:36px;">${data.temp}°C</p>
-                            <p style="font-size:14px;">feels like: ${data.feels_like}°C</p>
-                            <p style="font-size:14px;">${data.description}</p>
+                              <p style="font-size:36px;">${data.temp}°C</p>
+                              <p style="font-size:14px;">feels like: ${data.feels_like}°C</p>
+                              <p style="font-size:14px;">${data.description}</p>
                             </aside>
                             </div>
                             <p class="timer"></p>
-                            <p style="font-size:18px;">${dateW}</p>
-                            <p style="font-size:18px;">${data.city}</p>
+                            <p class="yearTimer"></p>
+                            <p style="font-size:18px;">${city}</p>
                             `
-    weatherBlock.querySelector('.timer').innerHTML = curr_h + ':' + curr_m + '<span class="timerSecond">:' + curr_s + "</span>"
-    timeInterval=setInterval(()=>{
-      curr_s++;
-      if (curr_s >= 60){
-        curr_s = 0
-        curr_m++
-      } else if (curr_m >= 60){
-        curr_m = 0
-        curr_h++
-      } else if ( curr_h >= 24 ){
-        curr_h = 0
+    return weatherBlock
+  }
+
+  headFunc(){
+    this.weatherBlock.querySelector('.anchorExtBtn').addEventListener('click', ()=>{
+      if(this.weatherBlock.className=="weatherBlockExt")
+        this.weatherBlock.className="weatherBlock"
+      else{
+        this.weatherBlock.className="weatherBlockExt"
+        this.weatherBlock.style.cssText = ''
       }
-      weatherBlock.querySelector('.timer').innerHTML = curr_h + ':' + curr_m + '<span class="timerSecond">:' + curr_s + "</span>"
-    }, 1000 )
-    weatherBlock.querySelector('.anchorExtBtn').addEventListener('click', ()=>{
-      weatherBlock.className=="weatherBlockExt"?weatherBlock.className="weatherBlock":weatherBlock.className="weatherBlockExt"
     })
-    weatherBlock.querySelector('.anchorBtn').addEventListener('click', ()=>cloceWeatherComponent(weatherBlock, timeInterval))
+    if(this.width>=1000){
+    this.weatherBlock.querySelector('.anchorDrgBtn').addEventListener('click', ()=>{
+      this.dragableFunc = !this.dragableFunc
+      if(this.dragableFunc)
+        this.dragable(this.weatherBlock)
+      else if(!this.dragableFunc || this.weatherBlock.className=="weatherBlockExt")
+        this.weatherBlock.style.cssText = ''
+    })}
+    this.weatherBlock.querySelector('.anchorBtn').addEventListener('click', ()=>this.closeElement(this.weatherBlock, this.timeInterval))
+  }
+
+  dragable(object){
+    if(object.className=="weatherBlock")
+      object.style.position='absolute'
+    this.dragValue
+    this.move=false
+    object.onmousedown=()=>{
+      this.dragValue=object
+      this.move=true
+    }
+    object.onmouseup=(e)=>{
+      this.dragValue=null
+      this.move=false
+    }
+    object.onmousemove=(e)=>{
+      if(this.move && object.className=="weatherBlock" && this.dragableFunc){
+        let x=e.pageX
+        let y=e.pageY
+        this.dragValue.style.left=x+"px"
+        this.dragValue.style.top=y+"px"
+      }
+    }
+  }
+
+  timer(object, dataTime){
+
+    this.curr_date = dataTime.getDate()
+    this.curr_month = dataTime.getMonth() + 1
+    this.curr_year = dataTime.getFullYear()
+    this.curr_h = dataTime.getHours()
+    this.curr_m = dataTime.getMinutes()
+    this.curr_s = dataTime.getSeconds()
+
+    object[1].innerHTML = `${this.curr_date}/${this.curr_month}/${this.curr_year}`
+
+    object[0].innerHTML = this.curr_h + ':' + this.curr_m + '<span class="timerSecond">:' + this.curr_s + "</span>"
+    this.timeInterval=setInterval(()=>this.tick(object[0]), 1000)
+  }
+
+  tick(object){
+    this.curr_s++;
+      if (this.curr_s >= 60){
+        this.curr_s = 0
+        this.curr_m++
+      } else if (this.curr_m >= 60){
+        this.curr_m = 0
+        this.curr_h++
+      } else if (this.curr_h >= 24 ){
+        this.curr_h = 0
+      }
+    object.innerHTML = this.curr_h + ':' + this.curr_m + '<span class="timerSecond">:' + this.curr_s + "</span>"
+  }
+
+  closeElement(object, timeInterval){
+    clearInterval(timeInterval)
+    object.remove()
   }
 }
-
-function cloceWeatherComponent(object, timeInterval){
-  clearInterval(timeInterval)
-  object.remove()
-}
-
-document.querySelector(".weather_hiden").addEventListener("dblclick",()=>{
-  isWeatherBlock = !isWeatherBlock
-  if(isWeatherBlock) weatherElement()
-})
-document.querySelector(".weather_hiden").addEventListener('touchend', ()=>{
-  isWeatherBlock = !isWeatherBlock
-  if(isWeatherBlock) weatherElement()
-})
 
 invColor = ['0','1']
 let i=1

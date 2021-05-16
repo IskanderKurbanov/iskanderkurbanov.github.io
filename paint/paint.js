c0c031bd3e91fd5706316511b25defb570d02ea7
IskanderKurbanov <58043802+IskanderKurbanov@users.noreply.github.com>
2021-05-17 02:44:23 +0300

    v2021.4.51

--- paint/paint.js
+++ paint/paint.js
@@ -1,56 +1,52 @@
 window.addEventListener('load', ()=>{
-	const paint = document.querySelector('.paintTest')
 
+	const paint = document.querySelector('.paintTest')
 	const canvas = paint.querySelector("#canvas")
 	const cntx = canvas.getContext('2d')
+	
+	let painting = false
+	let color = 'black'
+
 	// resizing
 	canvas.height = 450
 	canvas.width =  450
 
-	let painting = false
-	let color = 'black'
+	// FUNCS
 
-	clearCanvas()
-
+	// func for painting
 	function startPosition(e){
 		painting = true
 		draw(e)
 	}
 
+	// func for painting
 	function endPosition(){
 		painting = false
 		cntx.beginPath()
 	}
 
+	// func for painting
 	function draw(e){
 		if(!painting) return
 
 		cntx.lineWidth = paint.querySelector('.pointerSize').value
 		cntx.lineCap = 'round'
-		cntx.strokeStyle = paint.querySelector('.colorPicker2').value
+		cntx.strokeStyle = paint.querySelector('.colorPicker').value
 
 		cntx.lineTo(e.layerX, e.layerY)
 		cntx.stroke()
-
 		cntx.beginPath()
 		cntx.moveTo(e.layerX, e.layerY)
 	}
-
-	paint.querySelector('.clear').addEventListener('click', ()=>clearCanvas())
-	paint.querySelector('.save').addEventListener('click', ()=>saveTest())
-
-	paint.querySelectorAll('.color').forEach(i=>{
-		i.addEventListener('click', ()=>{
-			clearColorTool()
-			color = i.getAttribute('data')
-			i.style.cssText = 'border:1px solid #000;'
-		})
-	})
-
-	canvas.addEventListener('pointerdown', startPosition)
-	canvas.addEventListener('pointerup', endPosition)
-	canvas.addEventListener('pointermove', draw)
-
+/*
+	function resizeCanvas(data){
+		let w2, h2
+		if(data=='minus') w2= canvas.width*2, h2= canvas.height*2;
+		else w2= canvas.width/2, h2= canvas.height/2;
+		canvas.width= w2;
+		canvas.height= h2;
+	}
+*/
 	function saveTest(){
 		let dataURL = canvas.toDataURL("image/png");
 		let newTab = window.open('about:blank','image from canvas');
@@ -59,13 +55,24 @@ window.addEventListener('load', ()=>{
 
 	function clearCanvas(){
 		cntx.clearRect(0, 0, canvas.width, canvas.height);
+
 		cntx.fillStyle = 'white'
 		cntx.fillRect(0,0,canvas.width, canvas.height)
 	}
-
-	function clearColorTool(i){
-		paint.querySelectorAll('.color').forEach(i=>{
-			i.style.cssText = 'border:0px'
+/*
+	paint.querySelectorAll('.scaleCanvas').forEach(i=>{
+		i.addEventListener('click', ()=>{
+			resizeCanvas(i.getAttribute('data'))
 		})
-	}
+	})
+*/
+	paint.querySelector('.pointerSize').oninput = ()=>paint.querySelector('.sizeLable').innerHTML = paint.querySelector('.pointerSize').value
+	paint.querySelector('.clear').addEventListener('click', clearCanvas)
+	paint.querySelector('.save').addEventListener('click', saveTest)
+
+	canvas.addEventListener('pointerdown', startPosition)
+	canvas.addEventListener('pointerup', endPosition)
+	canvas.addEventListener('pointermove', draw)
+
+	clearCanvas()
 })
